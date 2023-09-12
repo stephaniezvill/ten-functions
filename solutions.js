78549c6394836ed008d90d934ee26e8f03ac7231
StephanieVillarreal <stephanie_zoz_villarreal@gmail.com>
2023-09-12 15:06:19 -0500

    solved problem number 6

--- solutions.js
+++ solutions.js
@@ -24,17 +24,28 @@
 function isTrue(input) {
     return input === true;
 }
+
 function isFalse(input) {
     return input === false;
 }
+
 function not(input) {
  return !input
 }
+
 function addOne(input) {
     return parseFloat(input) +1;
 }
+
 function isEven (input) {
     if (input === false) {
         return false;
     }
     return (input % 2) === 0
+}
+
+function isIdentical (input1, input2) {
+   return input1 === input2
+}
+
+function
